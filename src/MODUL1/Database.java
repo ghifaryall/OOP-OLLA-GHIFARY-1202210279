8d31e596eca22e3656337427310b533a4b50c511
ghifaryall <116285493+ghifaryall@users.noreply.github.com>
2022-11-01 16:07:38 +0700

    Add files via upload

---
+++ src/MODUL1/Database.java
@@ -0,0 +1,21 @@
+package MODUL1;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+    nasi
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+    public void Insert Menu (String[Menu]args){
+        
+    }
+    }
+
+    // TODO Create Method to Insert Menu to Database
+
+    // TODO Create Method to Show Menu from Database
+
+    // TODO Create Method to Search Menu from Database
+
+
+}
---
+++ src/MODUL1/Menu.java
@@ -0,0 +1,17 @@
+package MODUL1;
+
+public class Menu {
+    String name;
+    String category;
+    String price;
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+    public void setname(String name ){
+        this.name = name;
+    }
+    public void setcategory(String category){
+        this.category = category;
+    }
+    public void setprice(String price){
+        this.price = price;
+    }
+}
